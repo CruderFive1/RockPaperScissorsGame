fdfd7431453c50e000d5b3a842affcba08ea81b2
Adrian <senften@outlook.com>
2023-12-09 01:00:14 +0100

    Initial Commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Adrian Senften on 09.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
