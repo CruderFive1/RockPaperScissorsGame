8a29aeee264644b259b1bc9ecbead2a3218a29e5
Adrian <senften@outlook.com>
2023-12-09 02:28:10 +0100

    The game locig is not working as it should. when i have to win the game the locig doesn't see it as a win

--- RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/ContentView.swift
@@ -7,17 +7,87 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var randomNumber = Bool.random() ? 1 : 0
+    @State private var score: Int = 0
+    @State private var userChoise: Int = 0
+    @State private var computerChoise: Int = Int.random(in: 0...2)
+    
+    let gameOptions = ["Rock", "Paper", "Scissors"]
+    let winningMovces = ["Paper", "Scissors", "Rock"]
+    
+    
+    // var computerOption = gameOptions[computerChoise]
+    var winCondition: String {
+        if randomNumber == 1 {
+            return "win"
+        } else {
+            return "loose"
         }
-        .padding()
     }
+    
+    var body: some View {
+        NavigationStack {
+            var computerOption = gameOptions[computerChoise]
+            
+            VStack {
+                Spacer()
+                Text("Choose the correct answer")
+                Text("You have to \(winCondition) the game")
+                Spacer()
+                Text("The computer chooses \(computerOption)")
+                Spacer()
+                HStack{
+                    
+                    ForEach(0..<3) {number in Button {
+                        game(User: number, Computer: computerChoise, Result: randomNumber)
+                        print(number)
+                        refreshGame()
+                    } label: {
+                        Text("\(gameOptions[number])")
+                        
+                    }
+                    }
+                    
+                }
+                Text("\(gameOptions[computerChoise])")
+                
+                Text("\(score)")
+                Spacer()
+            }
+        }
+        
+    }
+    func game(User: Int, Computer: Int, Result: Int) {
+        if Result == 1 && gameOptions[Computer] == winningMovces[User] {
+            score += 1
+            print("Win")
+            print(winningMovces[User])
+            
+        } else if Result == 0 && gameOptions[Computer] == winningMovces[User] {
+            score += 1
+            print("Win")
+            print(winningMovces[User])
+            
+        } else {
+            print("You lose")
+            print(winningMovces[User])
+            
+        }
+        
+        print("User: \(User), Computer: \(Computer), Result: \(Result), Score: \(score)")
+    }
+    func refreshGame() {
+        randomNumber = Bool.random() ? 1 : 0
+        computerChoise = Int.random(in: 0...2)
+    }
+    
 }
+
+
+
 
 #Preview {
     ContentView()
