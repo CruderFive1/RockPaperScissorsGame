01200d262945c4bb13895c16b8b5d70d474dc644
Adrian <senften@outlook.com>
2023-12-10 17:07:21 +0100

    Background + Alert

--- RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
     @State private var score: Int = 0
     @State private var userChoise: Int = 0
     @State private var computerChoise: Int = Int.random(in: 0...2)
+    @State private var showingAlert = false
+    @State private var winOrLose = ""
     
     let gameOptions = ["Rock", "Paper", "Scissors"]
     let winningMovces = ["Paper", "Scissors", "Rock"]
@@ -31,13 +33,26 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             let computerOption = gameOptions[computerChoise]
+            ZStack {
+                LinearGradient(colors: [.white, .blue], startPoint: .bottom, endPoint: .top)
             
             VStack {
                 Spacer()
                 Text("Choose the correct answer")
-                Text("You have to \(winCondition) the game")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.black)
                 Spacer()
+               
+                    Text("You have to \(winCondition) the game")
+                            .font(.title2)
+                        .fontWeight(.heavy)
+                
+                
+                
                 Text("The computer chooses \(computerOption)")
+                    .font(.title2)
+                    .fontWeight(.medium)
+                    
                 Spacer()
                 HStack{
                     
@@ -45,18 +60,33 @@ struct ContentView: View {
                         game(User: number, Computer: computerChoise, Result: randomNumber)
                         print(number)
                         refreshGame()
+                        showingAlert = true
                     } label: {
                         Text("\(gameOptions[number])")
                         
                     }
+                    .buttonStyle(.borderedProminent)
                     }
                     
-                }
-                Text("\(gameOptions[computerChoise])")
                 
-                Text("\(score)")
+                   
+                }
+
+                
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text("\(winOrLose)"),
+                          message: Text("Your score is: \(score)"),
+                          dismissButton: .default(Text("Got it!")))
+
+                    
+                }
+
                 Spacer()
             }
+                
+
+            }.ignoresSafeArea()
+            
         }
         
     }
@@ -65,15 +95,18 @@ struct ContentView: View {
             score += 1
             print("Win")
             print(winningMovces[User])
+            winOrLose = "you Win"
             
         } else if Result == 0 && gameOptions[User] != winningMovces[Computer] {
             score += 1
             print("Win")
             print(winningMovces[User])
+            winOrLose = "you Win"
             
         } else {
             print("You lose")
             print(winningMovces[User])
+            winOrLose = "you loose"
             
         }
         
